3bae8118712b81aaf51cf07f9526ad81e99b5d60
Pavel <bpegirk@ya.ru>
2017-03-17 17:28:47 +0800

    + add watcher to file + add config file + parsing json to class
    TODO:
     - compile file by selected language
     - run tests
     - detect execute time and used memory
     - fill class with results
     - out result to folder

--- ProgramChecker/Program.cs
+++ ProgramChecker/Program.cs
@@ -2,13 +2,94 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using ProgramChecker.classes;
+using System.IO;
+using IniParser;
+using IniParser.Model;
+using Newtonsoft.Json;
 
 namespace ProgramChecker
 {
     class Program
     {
+        private static IniData globalConfig;
         static void Main(string[] args)
         {
+
+            var mainConfig = new IniData();
+            String currDir = Directory.GetCurrentDirectory() + "\\";
+            String configFile = currDir + "config.ini";
+            mainConfig["paths"]["src"] = currDir + "src\\";
+            mainConfig["paths"]["queue"] = currDir + "queue\\";
+            mainConfig["paths"]["results"] = currDir + "results\\";
+            // init config
+            var parser = new FileIniDataParser();
+            if (!File.Exists(configFile))
+            {
+                // write default setting               
+                parser.WriteFile(configFile, mainConfig);
+            }
+            IniData fileConfig = parser.ReadFile(configFile);
+            mainConfig.Merge(fileConfig);
+
+            globalConfig = mainConfig;
+
+            // create folders if not exists
+
+            if (!Directory.Exists(globalConfig["paths"]["src"]))
+            {
+                Directory.CreateDirectory(globalConfig["paths"]["src"]);
+            }
+
+            if (!Directory.Exists(globalConfig["paths"]["queue"]))
+            {
+                Directory.CreateDirectory(globalConfig["paths"]["queue"]);
+            }
+            if (!Directory.Exists(globalConfig["paths"]["results"]))
+            {
+                Directory.CreateDirectory(globalConfig["paths"]["results"]);
+            }
+            // run folder listiner
+            listenFolder();
+
+            // wait - not to end
+            new System.Threading.AutoResetEvent(false).WaitOne();
+        }
+
+        private static void listenFolder()
+        {
+            String srcFolder = globalConfig["paths"]["src"];
+            string queueFolder = globalConfig["paths"]["queue"];
+
+            FileSystemWatcher watcher = new FileSystemWatcher();
+
+            watcher.Path = queueFolder;
+            watcher.NotifyFilter = NotifyFilters.FileName;
+            watcher.Filter = "*.new";
+            watcher.Created += new FileSystemEventHandler(fileRecived);
+            watcher.EnableRaisingEvents = true;
+            Console.WriteLine("Start watching files in folder " + queueFolder + "...");
+        }
+
+        private static void fileRecived(object source, FileSystemEventArgs e)
+        {
+            Console.Write("File " + Path.GetFileName(e.FullPath) + " come. Try parse...");
+            // try parse file
+            try
+            {
+                String fileContent = File.ReadAllText(e.FullPath);
+                Check prarm = JsonConvert.DeserializeObject<Check>(fileContent);
+            }
+            catch (Exception ex)
+            {
+                Console.Write("Can't read file " + e.FullPath + ": " + ex.Message);
+            }
+            Console.WriteLine("Done");
+        }
+
+        private static void compileCode(Check param)
+        {
+
         }
     }
 }
---
+++ ProgramChecker/classes/Check.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProgramChecker.classes
+{
+    class Check
+    {
+        const int PASCAL = 1;
+        const int C_SHARP_2008 = 2;
+        const int VB_2008 = 3;
+        const int C_PLUS_2008 = 4;
+        const int DELPHI = 5;
+        const int C_BUILDER = 6;
+        const int C_SHARP_2013 = 7;
+        const int VB_2013 = 8;
+        const int C_PLUS_2013 = 9;
+
+
+        public int checkId { get; set; }
+        public int language { get; set; }
+        public string fileName { get; set; }
+        public int parseDec { get; set; }
+        public bool isChecked { get; set; }
+        public int passCount { get; set; }
+        public List<Test> tests { get; set; }
+    }
+
+    class Test
+    {
+        public int id { get; set; }
+        public int exerciseId { get; set; }
+        public string input { get; set; }
+        public string output { get; set; }
+        public float balls { get; set; }
+        public bool isPassed { get; set; }
+        public DateTime checkTime { get; set; }
+        public string compileError { get; set; }
+        public string result { get; set; }
+
+    }
+}
